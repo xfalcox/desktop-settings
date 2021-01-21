91c8ff76d6ead0fe6a70d81a03acc0f15891bb89
Jonas Strassel <info@jonas-strassel.de>
2021-01-21 09:31:23 +0100

    fix(sway): use class for floating kitty

--- community/sway/usr/share/sway/scripts/terminal.sh
+++ community/sway/usr/share/sway/scripts/terminal.sh
@@ -8,8 +8,8 @@ GL_VERSION=$(glxinfo | awk '/OpenGL version/ {print $4}')
 CMD=""
 if [[ -x "$(command -v kitty)" && $(version $GL_VERSION) -ge $(version "3.3") ]]; then
     CMD="kitty"
-    [ -n "$1" ] && [ "$1" == "float" ] && CMD="$CMD --name=floating_shell"
-    [ -n "$1" ] && [ "$1" == "float_portrait" ] && CMD="$CMD --name=floating_shell_portrait"
+    [ -n "$1" ] && [ "$1" == "float" ] && CMD="$CMD --class=floating_shell"
+    [ -n "$1" ] && [ "$1" == "float_portrait" ] && CMD="$CMD --class=floating_shell_portrait"
     [ -n "$1" ] && CMD="$CMD ${2:-$1}"
 else
     CMD="termite --config=\$theme/termite"
